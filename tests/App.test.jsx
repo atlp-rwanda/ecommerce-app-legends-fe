22683851faffc397a7fa6ea837fcf8b33f2df104
Fabrice <mwanafunzifabrice@gmail.com>
2023-04-18 23:51:33 +0200

    chore [ vitest setup ]
    - install vitest with its related dependencies
    - configure test within vit.consig.js
    - create a setupTests.js to clear tests
    - create a sample test to test a wellcome message
    - add coverage to display coverage reporter
    
    chore [ finish vitest setup]

---
+++ tests/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+describe('App', () => {
+  it('should have wellcome message', () => {
+    render(<App />);
+    expect(screen.getByText(/ecommerce legend/i))
+  })
+}) 
