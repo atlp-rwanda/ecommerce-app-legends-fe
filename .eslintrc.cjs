d711b64586577caeb7cb05fc6244b8d98b9cae0d
Fabrice <mwanafunzifabrice@gmail.com>
2023-04-18 23:51:33 +0200

    chore [ Configure ESLint and Git pre-commit hook]
    Configure ESLint to check for style violations and flag them,
    and set up a pre-commit hook to run the format command based on ESlint and Prettier rules before every commit,
    as well as to run tests to ensure that no code is committed with any test-breaking changes.
    
    chore [ vitest setup ]
    
    - install vitest with its related dependencies
    - configure test within vit.consig.js
    - create a setupTests.js to clear tests
    - create a sample test to test a wellcome message
    - add coverage to display coverage reporter
    
    chore [ finish vitest setup]
    
    ch[setup-styling-46] setup styling
    
    - install tailwindcss
    - install postcss
    - install autoprefixer
    - install postcss-loader
    
    ch[setup-styling-46] setup styling

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+  },
+};
