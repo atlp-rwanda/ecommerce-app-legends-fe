9b0aae046352229a3b59a29324f47cf44df6e24c
Abiyingoma <cabiyingoma@BNRW.LOCAL>
2023-04-19 15:19:16 +0200

    bg(eslint):fix
    - eslint rules fix
    
    [delivers #24562]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['airbnb', 'airbnb/hooks', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -11,11 +15,15 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 0,
-    'no-unused-vars': 'warn',
-    'no-undef': 'warn',
-    quotes: ['error', 'single', {
-      allowTemplateLiterals: true
-    }]
-  }
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 'error',
+    'react/no-unescaped-entities': 'error',
+    'no-param-reassign': 'error',
+  },
 };
--- src/redux/greeting.jsx
+++ src/redux/greeting.jsx
@@ -4,17 +4,19 @@ export const greetingSlice = createSlice({
   name: 'greeting',
   initialState: {
     greet: 'I can greet',
+    greet: 'I can greet',
   },
   // defining actions through reducer
   reducers: {
     greet: (state) => {
-      return { ...state, greet: 'hello devs' };
+      state.greet = 'hello devs';
     },
     sayBye: (state) => {
-      return { ...state, greet: 'bye bye happy coding' };
+      state.greet = 'bye bye happy coding';
     },
   },
 });
 // Action creators to begenerated for each case reducer function
+
 export const { greet, sayBye } = greetingSlice.actions;
 export default greetingSlice.reducer;
