eeabcce16c8abd12e2407fd9280f347af82dacb0
Christa <ishimwechristabellah1@gmail.com>
2023-05-29 14:51:28 +0200

    updating About Us and installing coverage dependencies and script

--- src/views/about/AboutPage.jsx
+++ src/views/about/AboutPage.jsx
@@ -9,34 +9,45 @@ const AboutPage = () => {
   return (
     <>
       <NavBar />
-      <div>
-        <section className="py-10">
+      <div className="flex flex-col min-h-screen">
+        <section className="py-10 flex-grow">
           <div className="container mx-auto px-4">
             <h1 className="text-3xl font-bold text-gray-800 m-10 text-center">
               {t('aboutUs')}
             </h1>
-            <p className="text-gray-700 mb-8 mx-[10vw]">{t('aboutUsText')}</p>
-            <h1 className="text-2xl font-normal text-gray-800 mb-4 text-center">
-              {t('ourMission')}
-            </h1>
-            <p className="text-gray-700 mb-8 mx-[10vw]">
-              {t('ourMissionText')}
-            </p>
-            <p className="text-gray-700 mb-8 mx-[10vw]">
-              <h1 className="text-2xl font-normal text-gray-800 mb-4 text-center">
-                {t('ourVision')}
-              </h1>
-              {t('ourVisionText')}
-            </p>
-            <p className="text-gray-700 mb-8 mx-[10vw]">
-              {t('ourVisionText2')}
-            </p>
+            <div className="flex justify-between">
+              <div className="w-1/3">
+                <h1 className="text-2xl font-normal text-gray-800 mb-4 text-center">
+                  {t('ourMission')}
+                </h1>
+                <p className="text-base text-gray-700 mb-8 mx-6 leading-7">
+                  {t('ourMissionText')}
+                </p>
+              </div>
+              <div className="w-1/3">
+                <h1 className="text-2xl font-normal text-gray-800 mb-4 text-center">
+                  {t('ourVision')}
+                </h1>
+                <p className="text-base text-gray-700 mb-8 mx-6 leading-7">
+                  {t('ourVisionText')}
+                </p>
+              </div>
+              <div className="w-1/3">
+                <h1 className="text-2xl font-normal text-gray-800 mb-4 text-center">
+                  {t('ourVision')}
+                </h1>
+                <p className="text-base text-gray-700 mb-8 mx-6 leading-7">
+                  {t('ourVisionText2')}
+                </p>
+              </div>
+            </div>
           </div>
         </section>
         <ChatButton />
+        <Footer className="mt-auto py-3" />
       </div>
-      <Footer />
     </>
   );
 };
+
 export default AboutPage;
